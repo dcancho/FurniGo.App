5c7f9f4acfe0be577349afb948770b55e7a2d604
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2023-09-29 17:43:51 -0500

    code: get buttons and modify

--- src/app/public/components/three-d-viewer/three-d-viewer.component.ts
+++ src/app/public/components/three-d-viewer/three-d-viewer.component.ts
@@ -6,23 +6,47 @@ import { Input } from '@angular/core';
   styleUrls: ['./three-d-viewer.component.css']
 })
 export class ThreeDViewerComponent {
-  @Input() itemTitle: string;
-  @Input() orderNumber: number;
-  @Input() status: string;
-  @Input() maxDate: Date;
-  @Input() linkToItem: string;
-  @Input() imgPath: string;
-  @Input() maxBudget: number;
-  @Input() userType: string;
 
-  constructor() { 
-    this.itemTitle = "Nombre del pedido";
-    this.orderNumber = 123456;
-    this.status = "En fabricación";
-    this.maxDate = new Date();
-    this.linkToItem = "/";
-    this.imgPath = "https://picsum.photos/200/300";
-    this.maxBudget = 250.00;
-    this.userType = "experto";
+  @Input() modelPath: string;
+  @Input() viewerType: string;
+
+  constructor() {
+    this.modelPath = 'assets/models/scene.gltf';
+    this.viewerType = 'view-model';
+    this.validateViewerType();
   }
+
+  // Verify that viewerType is either 'view-model' or 'edit-model'. If not, set it to 'view-model' before creating the viewer.
+
+  validateViewerType() {
+    if (this.viewerType !== 'view-model' && this.viewerType !== 'edit-model') {
+      this.viewerType = 'view-model';
+    }
+  }
+
+  checkStyles() {
+    if (this.viewerType === 'edit-model') {
+      // Hide the 'enlarge-view' button
+      const enlargeViewButton = document.querySelector('.enlarge-button');
+      if (enlargeViewButton) {
+        enlargeViewButton.classList.add('hidden');
+      }
+
+      // Set the other two buttons to use the 'primary action' class
+      const uploadButton = document.querySelector('.upload-button');
+      if (uploadButton) {
+        uploadButton.classList.remove('secondary-action');
+        uploadButton.classList.add('primary-action');
+      }
+
+      const downloadButton = document.querySelector('.download-button');
+      if (downloadButton) {
+        downloadButton.classList.remove('secondary-action');
+        downloadButton.classList.add('primary-action');
+      }
+    }
+  }
+
+
+
 }
